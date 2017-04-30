3976c49f7574335f88726c6dec01ab9a6a65c857
w014278e <w014278e@student.staffs.ac.uk>
2017-04-30 15:30:58 +0100

    test
    test

--- scripts.js
+++ scripts.js
@@ -75,13 +75,13 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const eventsContainer = document.getElementById('matchStats');
-if(matchStatsContainer){
+const eventsContainer = document.getElementById('events');
+if(eventsContainer){
     fetch("events.json")
         .then(response => {
             return response.json();
         }).then(events => {
-            const eventsHTML = matchStats.map(event => {
+            const eventsHTML = events.map(event => {
                 return `<div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
                     <div class="mdl-card__media">
                         <img class="article-image" src="${event.picture}" border="0" alt="">
@@ -98,7 +98,7 @@ if(matchStatsContainer){
                 </div>`;
             }).join("\n");
             
-            matchStatsContainer.innerHTML = eventsHTML;
+            eventsContainer.innerHTML = eventsHTML;
         });
 }
 
