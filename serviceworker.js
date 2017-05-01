ed0ef2d71cc15f803f23616c2a266df830fd9293
w014278e <w014278e@student.staffs.ac.uk>
2017-05-01 16:54:43 +0100

    updated service worker
    updated service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 var BASE_PATH = '/sportsappdinaldemo/';
-var CACHE_NAME = 'bgPWA-v12';
+var CACHE_NAME = 'bgPWA-v13';
 var TEMP_IMAGE_CACHE_NAME = 'temp-cache-v1';
 var footballheadlinesJSON = "https://newsapi.org/v1/articles?source=talksport&sortBy=top&apiKey=2117f72d54d5433eba479863fb7ab3e5";
 var CACHED_URLS = [
