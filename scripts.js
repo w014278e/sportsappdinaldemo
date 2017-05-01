539bba9232c3ac8cf153fb1eba6e741db186e021
w014278e <w014278e@student.staffs.ac.uk>
2017-05-01 17:02:56 +0100

    changed scripts for json

--- scripts.js
+++ scripts.js
@@ -75,13 +75,13 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const eventsContainer = document.getElementById('events');
+const eventsContainer = document.getElementById('footballLatestStats');
 if(eventsContainer){
     fetch("events.json")
         .then(response => {
             return response.json();
-        }).then(events => {
-            const eventsHTML = events.map(event => {
+        }).then(footballLatestStats => {
+            const eventsHTML = footballLatestStats.map(event => {
                 return `<div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
                     <div class="mdl-card__media">
                         <img class="article-image" src="${event.picture}" border="0" alt="">
@@ -98,7 +98,7 @@ if(eventsContainer){
                 </div>`;
             }).join("\n");
             
-            eventsContainer.innerHTML = eventsHTML;
+            eventsContainer.innerHTML = footballLatestStatsHTML;
         });
 }
 
